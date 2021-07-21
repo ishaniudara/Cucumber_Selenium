19f4825b1d1eb14c70bb6291abb8ee952c255ee5
Ishani udara <41565843+ishaniudara@users.noreply.github.com>
2021-07-21 14:57:30 +0530

    first

---
+++ steos.java
@@ -0,0 +1,24 @@
+package First;
+
+public class steos {
+	@Given(" Open chrome and strat the application$")
+	public void open_chrome() {
+		System.out.println("chrome");
+		
+	}
+	
+	
+	@When(" I enter valid username and valod password$")
+	public void enter_credentials() {
+		System.out.println("chrome");
+		
+	}
+	
+
+	@Then(" user should be able to login successfullg$")
+	public void login() {
+		System.out.println("chrome");
+		
+	}
+	
+}
---
+++ test.feature
@@ -0,0 +1,5 @@
+Feature : Test facebook login page
+Scenario: Test login with valid credintials
+Given Open chrome and strat the application
+When I enter valid username and valod password
+then user should be able to login successfullg
